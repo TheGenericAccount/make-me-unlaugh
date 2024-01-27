29c4c6f7798ec5fb2806a02ace37e8485e1a7031
pijus3002 <pijus3002@gmail.com>
2024-01-27 19:59:07 +0200

    UI

--- Levels/Button.gd
+++ Levels/Button.gd
@@ -1,23 +1,15 @@
 extends Button
 var was_pressed = false
 
-
 var game = preload("res://Levels/game.tscn")
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_pressed():
 	if !was_pressed:
-		$"../BlackScreen/AnimationPlayer".play("UnFade")
+		Transition.change_scene(game)
 		was_pressed = true
-		
-
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "UnFade":
---
+++ Levels/Transition.gd
@@ -0,0 +1,22 @@
+extends ColorRect
+var current_scene
+var changing_scene
+func change_scene(scene):
+	$AnimationPlayer.play("UnFade")
+	changing_scene=scene
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "UnFade":
+		current_scene.queue_free()
+		var instance = changing_scene.instantiate()
+		get_tree().get_root().add_child(instance)
+		get_parent().move_child.call_deferred(instance,0)
+		get_parent().move_child.call_deferred(self,1)
+		$AnimationPlayer.play("Fade")
+
+func _ready():
+	get_parent().move_child.call_deferred(self,1)
+
+
+
+
