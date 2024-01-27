01c5dd49c88c6e28ed08141b2a4742e5495d7d3a
pijus3002 <pijus3002@gmail.com>
2024-01-27 16:36:21 +0200

    city mood

---
+++ Environment/UI/CityMood.gd
@@ -0,0 +1,17 @@
+extends TextureRect
+
+const NEUTRAL_START = 30
+const NEUTRAL_END = 70
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ScoreCounter.timer < NEUTRAL_START:
+		$AnimationPlayer.play("Sad")
+	elif ScoreCounter.timer > NEUTRAL_END:
+		$AnimationPlayer.play("Happy")
+	else: $AnimationPlayer.play("Neutral")
---
+++ Environment/UI/ProgressTexture.gd
@@ -0,0 +1,17 @@
+class_name ScoreCounter
+extends TextureProgressBar
+
+const MULTIPLIER_BASE = 1
+static var timer:float = 0.00
+static var multiplier:float = MULTIPLIER_BASE
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	value=0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	timer+=delta*multiplier
+	value=timer
+	if timer < 20:
+		multiplier *= 1.01
