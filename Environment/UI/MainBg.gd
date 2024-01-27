5ba03bad9288a86c4cf7d1af83e30ef55050ad49
pijus3002 <pijus3002@gmail.com>
2024-01-27 21:59:41 +0200

    Supply change is life

--- Environment/UI/MainBg.gd
+++ Environment/UI/MainBg.gd
@@ -12,11 +12,18 @@ static var supply={
 	"dog-item":10,
 }
 
+func _on_supply_timer_timeout():
+	for item in supply:
+		if supply[item] >= 3:
+			supply[item]+=randi_range(-3,1)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$Supply.text=""
+	for item in supply:
+		$Supply.text+=str(supply[item])
+		$Supply.text+="\n"
