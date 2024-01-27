be324a5fe1aba9d2d41c016291a199140d742c6c
Dovydas Labanauskas <labanauskas.dovydas@gmail.com>
2024-01-27 10:11:35 +0200

    fixed infinite spawn bug

--- Levels/Spawner.gd
+++ Levels/Spawner.gd
@@ -5,6 +5,7 @@ extends Node2D
 @export var SPAWN_INTERVAL:float=5
 @export var MAX_TO_SPAWN:int=5
 @export var X_SPREAD:float=100
+var items_spawned=0
 
 func _ready():
 	$Timer.wait_time=SPAWN_INTERVAL
@@ -17,7 +18,7 @@ func _on_timer_timeout():
 	spawn()
 
 func spawn()->void:
-	if(get_child_count()>MAX_TO_SPAWN):
+	if(items_spawned>MAX_TO_SPAWN):
 		return
 	var item_parent=ItemParent.instance
 	var instance:Node2D=RESOURCE_TO_SPAWN.instantiate()
@@ -25,3 +26,5 @@ func spawn()->void:
 	instance.global_position.x+=randf_range(-X_SPREAD, X_SPREAD)
 	instance.global_rotation+=randf_range(0, 360)
 	item_parent.add_child(instance)
+	instance.tree_exiting.connect(func():items_spawned-=1)
+	items_spawned+=1;
