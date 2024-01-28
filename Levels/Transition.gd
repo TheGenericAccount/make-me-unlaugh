d1acf4d262b6c50dac43cfd0e993683493c23c36
pijus3002 <pijus3002@gmail.com>
2024-01-28 17:49:18 +0200

    intro

--- Levels/Transition.gd
+++ Levels/Transition.gd
@@ -8,8 +8,9 @@ func change_scene(scene):
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "UnFade":
 		current_scene.queue_free()
-		var instance = changing_scene.instantiate()
+		var instance = changing_scene
 		get_tree().get_root().add_child(instance)
+		current_scene = instance
 		get_parent().move_child.call_deferred(instance,0)
 		get_parent().move_child.call_deferred(self,1)
 		$AnimationPlayer.play("Fade")
